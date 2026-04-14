6655960a49f2b8f4913b275933a1f59cc94977b5
Duarte Cruz <duartecruz98@isr.uc.pt>
2026-04-14 21:37:38 +0100

    Add bunker_description and bunker_gazebo_sim (to use in gazebo simulations)

---
+++ shared_folder/bunker-launch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+#ROS 2 Middleware Implementation 
+
+#Fastrtps
+#export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+#export FASTDDS_BUILTIN_TRANSPORTS=UDPv4
+
+#Cyclonedds
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
+# Source ROS2 Jazzy
+source /opt/ros/jazzy/setup.bash
+
+#Build workspace only with the packages descriminated on docker compose file
+cd /root/ros2_ws/
+colcon build --symlink-install
+
+# Source ROS2 Workspace
+source /root/ros2_ws/install/setup.bash
+
+#Run realsense driver
+ros2 launch bunker_description robot_state_publisher.launch.py
